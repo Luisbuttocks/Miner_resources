6b8488ad9da7dcf005f5c61ed753ac8e4cc33e7c
Luisbuttocks <costomf00.04@gmail.com>
2026-01-26 20:02:14 +0530

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,1 @@
+wget https://gitlab.com/Kanedias/xmrig-static/-/releases/permalink/latest/downloads/xmrig-aarch64-static && chmod +x xmrig-aarch64-static && ./xmrig-aarch64-static
---
+++ script86.sh
@@ -0,0 +1,1 @@
+wget https://gitlab.com/Kanedias/xmrig-static/-/releases/permalink/latest/downloads/xmrig-x86_64-static && chmod +x xmrig-x86_64-static && ./xmrig-x86_64-static -c Config86.json
